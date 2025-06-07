6985e93ef73baceb63427e9ff4c5069b9ad5df8d
Gowri Shankara Narayanan <23f2001579@ds.study.iitm.ac.in>
2025-06-07 20:29:37 +0000

    Created Controllers, Database, models, venv

---
+++ controllers/models.py
@@ -0,0 +1,37 @@
+from .database import db #check for this file in the folder you are existing
+from datetime import datetime
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable = False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    type = db.Column(db.String, default="general")
+
+    reservations = db.relationship('Reservation', backref='user')
+
+class ParkingLot(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    prime_location_name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    pincode = db.Column(db.Integer,nullable = False)
+    maximum_spots = db.Column(db.Integer, nullable=False)
+
+    spots = db.relationship('ParkingSpot', backref='lot')
+
+class ParkingSpot(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'), nullable=False)
+    status = db.Column(db.String(1), nullable=False)
+
+    reservations = db.relationship('Reservation', backref='spot')
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    parking_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    leaving_timestamp = db.Column(db.DateTime, nullable=True)
+    parking_price = db.Column(db.Integer, nullable=False)
+
