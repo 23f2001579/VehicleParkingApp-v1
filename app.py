8c3a4601ff33c9f1ccc60972fe3bd9190d971da7
Gowri Shankara Narayanan <23f2001579@ds.study.iitm.ac.in>
2025-06-20 02:57:20 +0000

    Added new-lot and edit-lot pages to site

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from controllers.database import db
+from flask_login import LoginManager
 app = None
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "mysecretkey"
     app.debug = True
     app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///vehicleparkingapp.sqlite3"#3 database
     db.init_app(app)#3 database
@@ -13,5 +15,15 @@ def create_app():
 app = create_app()
 from controllers.controller import * #2 controllers
 # from application.models import #indirect connection using controllers.py
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"  # name of your login route
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
+    
 if __name__ == "__main__":
     app.run(debug = True)
