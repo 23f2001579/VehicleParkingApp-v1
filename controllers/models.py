27f02f63aa33599e3cf31024f8e3dd28c41510e3
Gowri Shankara Narayanan <23f2001579@ds.study.iitm.ac.in>
2025-06-21 17:38:23 +0000

    Modified model as per reservation requirement

--- controllers/models.py
+++ controllers/models.py
@@ -26,14 +26,15 @@ class ParkingSpot(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'), nullable=False)
     status = db.Column(db.String(1), nullable=False, default='A')
-
     reservations = db.relationship('Reservation', backref='spot')
 
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'), nullable = False)
     parking_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     leaving_timestamp = db.Column(db.DateTime, nullable=True)
     parking_price = db.Column(db.Integer, nullable=False)
+    vehicle_no = db.Column(db.String, nullable=False)
 
