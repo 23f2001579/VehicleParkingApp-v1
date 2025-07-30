eba6776b639be6dd1d943b6f2150453ae6ae08d7
Gowri Shankara Narayanan <23f2001579@ds.study.iitm.ac.in>
2025-07-30 17:08:28 +0000

    Fixing errors

--- app.py
+++ app.py
@@ -3,26 +3,30 @@ from controllers.database import db
 from flask_login import LoginManager
 app = None
 
+# use this to create database and kickstart myapp  
+# from app import *
+# db.create_all()
+
 def create_app():
     app = Flask(__name__)
     app.secret_key = "mysecretkey"
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///vehicleparkingapp.sqlite3"#3 database
-    db.init_app(app)#3 database
-    app.app_context().push() #runtime error, brings everything under context of flask applic
+    app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///vehicleparkingapp.sqlite3" #database is created
+    db.init_app(app)
+    app.app_context().push() #runtime error, brings everything under context of flask app
     return app
 
 app = create_app()
-from controllers.controller import * #2 controllers
-# from application.models import #indirect connection using controllers.py
+from controllers.controller import * 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"  # name of your login route
+login_manager.login_view = "login"  
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
+    #return User.query.get(int(user_id)) old one; but no errors
     
     
 if __name__ == "__main__":
